4da1da94692d95d3b3665b1d703cc7cb785bbef6
simon wilson <simonhanwilson@hotmail.co.uk>
2021-01-05 14:38:12 +0000

    complete

--- Worst Calculator/ViewController.swift
+++ Worst Calculator/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func addClicked(_ sender: Any) {
+        
+        if let firstNum = Int(firstNumber.text!) {
+        
+            if let secondNum = Int(secondNumber.text!) {
+        
+                let result = firstNum + secondNum
+                
+                resultLabel.text = String(result)
+                
+            }
+        
+        }
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        
+        if let firstNum = Int(firstNumber.text!) {
+        
+            if let secondNum = Int(secondNumber.text!) {
+        
+                let result = firstNum - secondNum
+                
+                resultLabel.text = String(result)
+                
+            }
+        
+        }
+        
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        
+        if let firstNum = Int(firstNumber.text!) {
+        
+            if let secondNum = Int(secondNumber.text!) {
+        
+                let result = firstNum * secondNum
+                
+                resultLabel.text = String(result)
+                
+            }
+        
+        }
+        
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        
+        if let firstNum = Int(firstNumber.text!) {
+        
+            if let secondNum = Int(secondNumber.text!) {
+        
+                let result = firstNum / secondNum
+                
+                resultLabel.text = String(result)
+                
+            }
+        
+        }
+    }
 }
 
